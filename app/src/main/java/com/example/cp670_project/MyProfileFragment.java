c0c4a054fa756b5b788c8f4c81c546b3ad51a561
gigio <gigioshobe21@gmail.com>
2021-08-08 10:35:08 -0400

    Fixed profile page sliding image

--- app/src/main/java/com/example/cp670_project/MyProfileFragment.java
+++ app/src/main/java/com/example/cp670_project/MyProfileFragment.java
@@ -7,10 +7,31 @@ import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
 
+import com.denzcoskun.imageslider.ImageSlider;
+import com.denzcoskun.imageslider.models.SlideModel;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class MyProfileFragment extends Fragment {
-    @Override
+
+    List<SlideModel> imageList;
+    ImageSlider carousel;
+
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         ViewGroup root = (ViewGroup) inflater.inflate(R.layout.my_profile_fragment, container, false);
+        carousel = root.findViewById(R.id.imageView2);
+
+        imageList = new ArrayList<SlideModel>(); // Create image list
+
+// imageList.add(SlideModel("String Url" or R.drawable)
+// imageList.add(SlideModel("String Url" or R.drawable, "title") You can add title
+
+        imageList.add(new SlideModel("https://bit.ly/37Rn50u", "Baby Owl"));
+        imageList.add(new SlideModel("https://bit.ly/3fLJf72", "The population of elephants is decreasing in the world."));
+        carousel.setImageList(imageList,true);
+
 
         return root;
     }
